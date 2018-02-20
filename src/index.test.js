8476db4d8eb84107db6e169a81352e44e33ad43d
Danny Martini <danny@wunderflats.com>
2018-02-20 14:32:00 +0100

    works

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,7 @@ describe("foo", () => {
   test("bar", done => {
     process.removeAllListeners("unhandledRejection");
     process.on("unhandledRejection", function(error) {
+      process.removeAllListeners("unhandledRejection");
       expect(error).toBeInstanceOf(Error);
       done();
     });
